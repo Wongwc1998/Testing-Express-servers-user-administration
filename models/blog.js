9e4681efad52fdb17e0b7ceb181eb90a0ab09ced
Wongwc1998 <wongwc1998@gmail.com>
2023-09-26 11:18:48 -0400

    change blog model

--- models/blog.js
+++ models/blog.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minlength: 5,
-    required: true
-  },
-  important: Boolean,
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
 blogSchema.set('toJSON', {
