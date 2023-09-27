4f14426916a03a93210508af674ba93845f0c83e
Wongwc1998 <wongwc1998@gmail.com>
2023-09-27 13:51:59 -0400

    add jwt

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,19 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -17,6 +28,8 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+      const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -26,6 +39,7 @@ blogsRouter.post('/', async (request, response) => {
   })
 
       const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
   response.status(201).json(savedBlog)
 })
 
