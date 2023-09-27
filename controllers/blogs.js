f556fd1ae118ef43d5bdc6d1a433ec6adb6cf2e5
Wongwc1998 <wongwc1998@gmail.com>
2023-09-27 19:59:53 -0400

    more lint

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const logger = require("../utils/logger");
 
 const jwt = require("jsonwebtoken");
 
--- mongo.js
+++ mongo.js
@@ -1,38 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://Fullstack:${password}@fullstackopen.yvhkeuh.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://Fullstack:${password}@fullstackopen.yvhkeuh.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
 const blog = new Blog({
-  title: 'The Art of War',
-  author: 'Sun Tzu',
-  url: 'https://en.wikipedia.org/wiki/The_Art_of_War',
-  likes: 100
-})
+  title: "The Art of War",
+  author: "Sun Tzu",
+  url: "https://en.wikipedia.org/wiki/The_Art_of_War",
+  likes: 100,
+});
 
-blog.save().then(result => {
-  console.log('blog saved!')
-  mongoose.connection.close()
-})
+blog.save().then((result) => {
+  console.log("blog saved!");
+  mongoose.connection.close();
+});
 
 // Blog.find({}).then(result => {
 //   result.forEach(blog => {
