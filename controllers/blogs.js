2031ce5b55d034240641694e34d32cbcdd6fbdc6
Wongwc1998 <wongwc1998@gmail.com>
2023-09-28 19:17:52 -0400

    refactor to middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,14 +5,6 @@ const logger = require("../utils/logger");
 
 const jwt = require("jsonwebtoken");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
@@ -30,18 +22,7 @@ blogsRouter.get("/:id", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   logger.info("Request to post a blog received.");
   const body = request.body;
-  const token = getTokenFrom(request);
-  if (!token) {
-    logger.warn("No token found in request.");
-    return response.status(401).json({ error: "Token missing or invalid" });
-  }
-
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, process.env.SECRET);
-  } catch (error) {
-    return response.status(401).json({ error: "Token missing or invalid" });
-  }
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   const user = await User.findById(decodedToken.id);
   logger.info({ user });
 
